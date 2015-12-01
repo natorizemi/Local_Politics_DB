15060f9fd654e19aa4ed3a9fb2fe258e4b28b957
ayamamo <8a.yamamo@gmail.com>
2015-12-02 00:22:53 +0900

    ミス修正

--- search_view.php
+++ search_view.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <!--<form action="search.php" method="POST">-->
-<form action-"word_cloud.php" method="POST
+<form action="word_cloud.php" method="POST">
   <input type="text" name="search">
   <input type="submit" value="検索">
 </form>
