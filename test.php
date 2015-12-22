83aa241bfccf5a08e9cf13943576b387ebeb3e52
taikikiki <me-rak-de-603@i.softbank.jp>
2015-12-22 13:44:44 +0900

    プルダウン
    地域選択→議員

--- test.php
+++ test.php
@@ -4,42 +4,77 @@
 </head>
 <body>
 
+
+
 <?php
 
 $link = mysql_connect('localhost', 'root', '');
 if (!$link) {
-    die('接続失敗です。'.mysql_error());
+    //die('接続失敗です。'.mysql_error());
 }
 
-print('<p>接続に成功しました。</p>');
+//rint('<p>接続に成功しました。</p>');
 
 $db_selected = mysql_select_db('2015_natori', $link);
 if (!$db_selected){
-    die('データベース選択失敗です。'.mysql_error());
+   //die('データベース選択失敗です。'.mysql_error());
 }
 
-print('<p>2015_natoriデータベースを選択しました。</p>');
+//print('<p>2015_natoriデータベースを選択しました。</p>');
 
 mysql_set_charset('utf8');
+$ken=htmlspecialchars($_POST['selectName1']);
+$shi=htmlspecialchars($_POST['selectName2']);
+$jis=$ken*1000+$shi;
+$jis=mb_convert_kana($jis,"a");
 
-$result = mysql_query("SELECT name FROM giin where detail='男'");
+
+
+//print('<p>');
+//echo $ken;
+//print('</p>');
+//print('<p>');
+//echo $shi;
+//print('</p>');
+//print('<p>');
+//echo $jis;
+//print('</p>');
+
+$result = mysql_query("SELECT name FROM giin where id like '%{$jis}%'");
 if (!$result) {
-    die('クエリーが失敗しました。'.mysql_error());
-}
-
-while ($row = mysql_fetch_assoc($result)) {
-    print('<p>');
-    
-    print($row['name']);
-    print('</p>');
-}
-
-$close_flag = mysql_close($link);
-
-if ($close_flag){
-    print('<p>切断に成功しました。</p>');
+    //die('クエリーが失敗しました。'.mysql_error());
 }
 
 ?>
+
+<form name="form1" method="post" action="test1.php">
+<?php
+while ($row = mysql_fetch_assoc($result)) {
+	$name=$row['name'];
+ 	
+ 	print('<p>');
+
+ 	echo "<td>
+	<a href='javascript:document.form.submit();' ></a>
+ 	<input type='submit' name='name' value='$name'>
+ 	</td>";
+ 	echo "<tr>";
+
+	print('</p>');
+	
+}
+?>
+</form>
+
+
+<?php
+$close_flag = mysql_close($link);
+
+if ($close_flag){
+    //print('<p>切断に成功しました。</p>');
+}
+
+?>
+
 </body>
-</html>
+</html> 
